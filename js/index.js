e10832cb4826b9bd2889dca1262158b9a94ae283
sirine21 <75286405+sirine21@users.noreply.github.com>
2021-04-29 15:01:08 -0700

    homepage 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 console.log("Your index.js file is loaded correctly!");
 
-
-$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+$(document).ready(function(){
+  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
      var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -22,3 +22,5 @@ $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
       }
     }
   });
+
+})
